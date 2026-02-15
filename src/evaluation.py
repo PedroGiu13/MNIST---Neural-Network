422b65fc5c12c7e1809b7ed7664fa3e03baec0a6
PedroGiu13 <pgiussani.e@gmail.com>
2026-02-15 01:07:13 +0000

    evaluation script

--- src/evaluation.py
+++ src/evaluation.py
@@ -4,12 +4,16 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.datasets import mnist
 
 # Load .h5 file of arbitrary name for testing (last if more than one)
-print(os.getcwd())
-for file in os.listdir(os.getcwd()):
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # path to src/
+PROJECT_ROOT = os.path.dirname(BASE_DIR)  # go up one level
+MODEL_DIR = os.path.join(PROJECT_ROOT, "models")
+
+print("Looking for models in:", MODEL_DIR)
+
+for file in os.listdir(MODEL_DIR):
     if file.endswith(".h5"):
-        print(file)
-        net = load_model(file)
-net.summary()
+        print("Loading:", file)
+        net = load_model(os.path.join(MODEL_DIR, file))
 
 
 # Determine what type of network this is
@@ -29,4 +33,5 @@ else:
 outputs = net.predict(x_test)
 labels_predicted = np.argmax(outputs, axis=1)
 correct_classified = np.sum(labels_predicted == labels_test)
+print(f"ANN = {netType}")
 print("Percentage correctly classified MNIST =", 100 * correct_classified / labels_test.size)
